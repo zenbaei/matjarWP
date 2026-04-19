73cd588716f1e289d1c3c5b66b8bb9bf1fbb1d63
islam <zenbaei@gmail.com>
2026-04-19 11:36:37 +0000

    remove uneeded code from functions.php

--- themes/xstore-child/functions.php
+++ themes/xstore-child/functions.php
@@ -6,20 +6,6 @@ function theme_enqueue_styles() {
 	}
 }
 
-## No need for it after adding wp_doing_ajax func
-function n2t_text_strings( $translated_text, $text, $domain ) {
-    switch ( $translated_text ) {
-		case 'SUBTOTAL:': // Original text
-            $translated_text = 'الإجمالي'; // Your translation
-            break;
-		case 'LOGIN:': // Original text
-            $translated_text = 'دخول'; // Your translation
-            break;
-    }
-    return $translated_text;
-}
-add_filter( 'gettext', 'n2t_text_strings', 20, 3 );
-
 // Switching locale on ajax calls
 add_action( 'init', function () {
     if ( wp_doing_ajax() ) {
